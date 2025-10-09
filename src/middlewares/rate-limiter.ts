d95675b753873c03c0613be1cbe26dfb4b45538d
Justin Rhodes <justinr@tri.be>
2025-10-09 09:40:02 -0500

    feat: implement rate limiting and update server configuration

---
+++ src/middlewares/rate-limiter.ts
@@ -0,0 +1,15 @@
+import { cloudflareRateLimiter } from "@hono-rate-limiter/cloudflare";
+
+export type AppType = {
+  Variables: {
+    rateLimit: boolean;
+  };
+  Bindings: {
+    SERVER_RATE_LIMITER: RateLimit;
+  };
+};
+
+export const rateLimit = cloudflareRateLimiter<AppType>({
+  rateLimitBinding: c => c.env.SERVER_RATE_LIMITER,
+  keyGenerator: c => c.req.header("cf-connecting-ip") ?? "", // Method to generate custom identifiers for clients.
+});
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 
 import categories from "./categories/categories.controller";
+import { rateLimit } from "./middlewares/rate-limiter.js";
 
 const app = new Hono().basePath("/api");
 
 app
+  .use(rateLimit)
   .get("/", (c) => {
     return c.text("Hello Hono!");
   })
