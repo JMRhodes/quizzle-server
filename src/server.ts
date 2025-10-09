eec89e979e6c9918c68f79c048c5b1acda719c58
Justin Rhodes <justinr@tri.be>
2025-10-09 14:28:13 -0500

    feat: add basic authentication middleware to the server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
 import { Hono } from "hono";
 import { pinoLogger } from "hono-pino";
+import { basicAuth } from "hono/basic-auth";
 
 import categories from "./categories/categories.controller";
 import { rateLimiter } from "./middlewares/rate-limiter.js";
 
-const app = new Hono().basePath("/api");
+type Bindings = {
+  USERNAME: string;
+  PASSWORD: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>().basePath("/api");
 
 app
+  .use("*", async (c, next) => {
+    const auth = basicAuth({
+      username: c.env.USERNAME,
+      password: c.env.PASSWORD,
+    });
+    await auth(c, next);
+  }) // Apply basic authentication
   .use(pinoLogger()) // Log all requests
   .use(rateLimiter); // Apply rate limiting middleware
 
